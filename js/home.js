33951f5d513559d435ddcab2a85b0147c588fae2
hungnm <hungnm2904@gmail.com>
2017-01-13 21:51:17 +0700

    Add menu for Shop navigator

---
+++ js/home.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    var owl = $("#owl-demo");
+
+    owl.owlCarousel({
+        items: 3, //10 items above 1000px browser width
+        itemsDesktop: [1000, 3], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+        itemsTablet: [600, 1], //2 items between 600 and 0
+        itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+        pagination: false
+    });
+    owl.trigger('owl.play', 3000);
+
+    // Custom Navigation Events
+    $(".next").click(function () {
+        owl.trigger('owl.next');
+    });
+    $(".prev").click(function () {
+        owl.trigger('owl.prev');
+    });
+
+    $('#nav-shop').hover(function () {
+        $('#nav-shop').parent().addClass('active')
+        $('#shop-menu').css('display', 'block');
+    });
+    $(document).on('mousemove', function(e) {
+        if(e.target.id !== 'nav-shop' && !$(e.target).closest('#shop-menu')[0]) {
+            $('#nav-shop').parent().removeClass('active')
+            $('#shop-menu').css('display', 'none');
+        }
+    });
+});
