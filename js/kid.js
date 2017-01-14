23c47fa0f14fa2ae8b44b256833754d671b255db
HuyTCM <huytcm.1708@gmail.com>
2017-01-14 13:17:47 +0700

    add model for kid and women

--- js/kid.js
+++ js/kid.js
@@ -13,9 +13,57 @@ var KID_ITEMS = {
             sandals: []
         },
         accessories: {
-            bags: [],
-            hats: [],
-            socks: []
+            bags: [
+                {
+                    name:"Media Cinch Sack",
+                    branch:"NEW BALANCE",
+                    shortDesc:"",
+                    price:'650.000',
+                    color:"Galaxy with Bright Cherry & Guava",
+                    description: "",
+                    img: [
+                        {
+                            src:'BAG_NEWBALANCE_1.jpg'
+                        },
+                        {
+                            src:'BAG_NEWBALANCE_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            hats: [
+                {
+                    name:"MESSI CAP",
+                    branch:"ADIDAS",
+                    shortDesc:"",
+                    price:'350.000',
+                    color:"BLACK/SOLRED (AP8924)",
+                    description: "",
+                    img: [
+                        {
+                            src:'HAT_ADIDAS_1.jpg'
+                        },
+                        {
+                            src:'HAT_ADIDAS_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            socks: [
+                {
+                    name:"GRAPHIC SOCKS 2 PAIRS",
+                    branch:"ADIDAS",
+                    shortDesc:"",
+                    price:'260.000',
+                    color:"Color Blue/Collegiate Navy/Black/White (AY6551)",
+                    description: "",
+                    img: [
+                        {
+                            src:'SOCKS_ADIDAS.jpg'
+                        }
+                    ]
+                }
+            ]
         },
         apparel: {
             tshirt: [],
@@ -35,9 +83,60 @@ var KID_ITEMS = {
             sandals: []
         },
         accessories: {
-            bags: [],
-            hats: [],
-            socks: []
+            bags: [
+                {
+                    name:"STUDIO BAG",
+                    branch:"NEW BALANCE",
+                    shortDesc:"",
+                    price:'2.500.000',
+                    color:"Urchin with Galapagos & Reef",
+                    description: "",
+                    img: [
+                        {
+                            src:'BAGS_Newbalance_1.jpg'
+                        },
+                        {
+                            src:'BAGS_Newbalance_1.jpg'
+                        }
+                    ]
+                }
+            ],
+            hats: [
+                {
+                    name:"KIDS SWIMMING",
+                    branch:"ADIDAS",
+                    shortDesc:"INFINITEX SWIM CAP",
+                    price:'390.000',
+                    color:"Color Bold Pink/White (M66935)",
+                    description: "",
+                    img: [
+                        {
+                            src:'SWIM CAP_Adidas_1.jpg'
+                        },
+                        {
+                            src:'SWIM CAP_Adidas_2.jpg'
+                        },
+                        {
+                            src:'SWIM CAP_Adidas_3.jpg'
+                        }
+                    ]
+                }
+            ],
+            socks: [
+                {
+                    name:"GIRLS TRAINING",
+                    branch:"ADIDAS",
+                    shortDesc:"FINDING NEMO SOCKS 3 PAIRS",
+                    price:'250.000',
+                    color:"Color Light Blue/Ice Mint/Semi Flash Orange/White (AY6475)",
+                    description: "",
+                    img: [
+                        {
+                            src:'SOCKS_Adidas_1.jpg'
+                        }
+                    ]
+                }
+            ]
         },
         apparel: {
             tshirt: [],
--- js/women.js
+++ js/women.js
@@ -12,9 +12,60 @@
             sandals:[]
         },
         accessories: {
-            bags:[],
-            hats:[],
-            socks:[]
+            bags:[
+                {
+                    name:"NIKE GYM CLUB",
+                    branch:"NIKE",
+                    shortDesc:"WOMEN'S TRAINING DUFFEL BAG",
+                    price:'1.090.000',
+                    color:"Hyper Pink/Black/Hyper Turquoise",
+                    description: "",
+                    img: [
+                        {
+                            src:'BAGS_Nike_1.jpg'
+                        },
+                        {
+                            src:'BAGS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            hats:[
+                {
+                    name:"NIKE AEROBILL",
+                    branch:"NIKE",
+                    shortDesc:"WOMEN'S RUNNING HAT",
+                    price:'690.000',
+                    color:"Bright Melon/Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'HATS_Nike_1.jpg'
+                        },
+                        {
+                            src:'HATS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            socks:[
+                {
+                    name:"NIKEGRIP LIGHTWEIGHT NO-SHOW",
+                    branch:"NIKE",
+                    shortDesc:"TRAINING SOCKS",
+                    price:'790.000',
+                    color:"Bright Melon/Medium Blue",
+                    description: "",
+                    img: [
+                        {
+                            src:'SOCKS_Nike_1.jpg'
+                        },
+                        {
+                            src:'SOCKS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ]
         },
         apparel:{
             sportbra: [],
