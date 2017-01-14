f820ea1f8f1f37cf97c05eedee947c478f8852f8
HuyTCM <huytcm.1708@gmail.com>
2017-01-14 12:12:36 +0700

    transit to detail page

---
+++ js/detail.js
@@ -0,0 +1,52 @@
+/**
+ * Created by HuyTCM on 1/14/17.
+ */
+(function () {
+
+    $(document).ready(function () {
+        "use strict";
+        // get url param
+        fillData();
+    });
+    function fillData() {
+        var id = findGetParameter('id');
+        var category = findGetParameter('category');
+        var type = findGetParameter('type');
+        var index = findGetParameter('index');
+        var model;
+        var imgPath = 'images/';
+        if (id == 1) {
+            // men
+            model = MEN_ITEM;
+            imgPath += 'men/';
+        } else if (id == 2) {
+            // women
+            model = WOMEN_ITEM;
+            imgPath += 'women/';
+        } else if (id == 3) {
+            // kid
+            var gender = findGetParameter('gender');
+            if (gender == 1) {
+                model = KID_ITEMS.boy;
+            } else {
+                model = KID_ITEMS.girl;
+            }
+            imgPath += 'kid/';
+        }
+
+        var item = model[category][type][index];
+        showData(item, imgPath);
+    }
+
+    function showData(item, imgPath) {
+        var imgSrc = imgPath + item.img[0].src;
+        var reviewImg = $('<img>');
+        reviewImg.attr('src', imgSrc);
+        $('#review-img').append(reviewImg);
+
+        $('#product-name').text(item.name);
+        $('#product-shortDesc').text(item.shortDesc);
+        $('#product-price').text(item.price);
+        $('#product-color').text(item.color);
+    }
+}).call();
