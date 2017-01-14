f19871e0fcb566f189ef31e9874ae5d16edd9492
HuyTCM <huytcm.1708@gmail.com>
2017-01-14 09:31:10 +0700

    edit Text something in footer and create data model

--- js/men.js
+++ js/men.js
@@ -1,3 +1,405 @@
 /**
  * Created by HuyTCM on 1/14/17.
  */
+(function () {
+
+    var MEN_ITEM = {
+        shoes : {
+            soccer:[
+                {
+                    name: "NIKE MERCURIAL SUPERFLY V FG",
+                    branch: "NIKE",
+                    shortDesc: "MEN'S FIRM-GROUND FOOTBALL BOOT",
+                    price:"8.590.000",
+                    color:"Purple Dynasty",
+                    description:"",
+                    img:[
+                        {
+                            src: 'SOCCER_Nike_1.jpg'
+                        },
+                        {
+                            src: 'SOCCER_Nike_2.jpg'
+                        },
+                        {
+                            src: 'SOCCER_Nike_3.jpg'
+                        }
+                    ]
+                },
+                {
+                    name: "Furon Pro FG",
+                    branch: "NEW BALANCE",
+                    shortDesc:"",
+                    price:'3.450.000',
+                    color:"Black with Impulse",
+                    description: "",
+                    img:[
+                        {
+                            src: 'SOCCER_NEWBALANCE_1.jpg'
+                        },
+                        {
+                            src: 'SOCCER_NEWBALANCE_2.jpg'
+                        }
+                    ]
+                },
+                {
+                    name:"ACE 16.3 PRIMEMESH TURF SHOES",
+                    branch:"ADIDAS",
+                    shortDesc:"",
+                    price:'2.700.000',
+                    color:"White/Gold Met./Core Black (AQ3432)",
+                    description: "",
+                    img: [
+                        {
+                            src:'SOCCER_ADIDAS_2.jpg'
+                        },
+                        {
+                            src:'SOCCER_ADIDAS_3.jpg'
+                        }
+                    ]
+                },
+                {
+                    name:"DS LIGHT X-FLY 2 MS",
+                    branch:"ASICS",
+                    shortDesc:"",
+                    price:'4.025.000',
+                    color:"FLASH YELLOW/BLACK",
+                    description: "",
+                    img: [
+                        {
+                            src:'SOCCER_ASICS_1.jpg'
+                        },
+                        {
+                            src:'SOCCER_ASICS_2.jpg'
+                        },
+                        {
+                            src:'SOCCER_ASICS_3.jpg'
+                        }
+                    ]
+                }
+            ],
+            baseketball:[
+                {
+                    name:"NIKE ZOOM REV EP",
+                    branch:"NIKE",
+                    shortDesc:"MEN'S BASKETBALL SHOE",
+                    price:'3.090.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'BASKETBAL_Nike_1.jpg'
+                        },
+                        {
+                            src:'BASKETBAL_Nike_2.jpg'
+                        },
+                        {
+                            src:'BASKETBAL_Nike_3.jpg'
+                        }
+                    ]
+                },
+                {
+                    name:"NEW BALANCE 581",
+                    branch:"NEW BALANCE",
+                    shortDesc:"",
+                    price:'1.380.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'BASKETBALL_NEW_1.jpg'
+                        },
+                        {
+                            src:'BASKETBALL_NEW_2.jpg'
+                        },
+                        {
+                            src:'BASKETBALL_NEW_3.jpg'
+                        }
+                    ]
+                },
+                {
+                    name:"COURT FURY 2016 MID SHOES",
+                    branch:"ADIDAS",
+                    shortDesc:"",
+                    price:'2.300.000',
+                    color:"FRED/BLK/WHT - 602",
+                    description: "",
+                    img: [
+                        {
+                            src:'BASKETBALL_ADIDAS_1.jpg'
+                        },
+                        {
+                            src:'BASKETBALL_ADIDAS_2.jpg'
+                        },
+                        {
+                            src:'BASKETBALL_ADIDAS_3.jpg'
+                        }
+                    ]
+                },
+                {
+                    name:"MEN’S CAGE",
+                    branch:"FILA",
+                    shortDesc:"",
+                    price:'2.070.000',
+                    color:"FILANAVY/FILARD - 431",
+                    description: "",
+                    img: [
+                        {
+                            src:'BASKETBALL_FILA_1.jpg'
+                        },
+                        {
+                            src:'BASKETBALL_FILA_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            tennis:[
+                {
+                    name:"NIKECOURT ZOOM VAPOR 9.5 TOUR",
+                    branch:"NIKE",
+                    shortDesc:"MEN'S TENNIS SHOE",
+                    price:'3.990.000',
+                    color:"Volt",
+                    description: "",
+                    img: [
+                        {
+                            src:'TENNIS_Nike_1.jpg'
+                        },
+                        {
+                            src:'TENNIS_Nike_2.jpg'
+                        },
+                        {
+                            src:'TENNIS_Nike_3.jpg'
+                        }
+                    ]
+                }
+            ],
+            training:[
+                {
+                    name:"NIKE ZOOM TRAIN COMPLETE",
+                    branch:"NIKE",
+                    shortDesc:"MEN'S TRAINING SHOE",
+                    price:'4.290.000',
+                    color:"Cool Grey",
+                    description: "",
+                    img: [
+                        {
+                            src:'TRAINING_Nike_1.jpg'
+                        },
+                        {
+                            src:'TRAINING_Nike_2.jpg'
+                        },
+                        {
+                            src:'TRAINING_Nike_3.jpg'
+                        }
+                    ]
+                }
+            ],
+            running:[
+                {
+                    name:"NIKE FREE RN MOTION FLYKNIT",
+                    branch:"NIKE",
+                    shortDesc:"MEN'S RUNNING SHOE",
+                    price:'4.290.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'RUNNING_Nike_1.jpg'
+                        },
+                        {
+                            src:'RUNNING_Nike_2.jpg'
+                        },
+                        {
+                            src:'RUNNING_Nike_3.jpg'
+                        }
+                    ]
+                }
+            ],
+            sandals:[
+                {
+                    name:"APPALACHIAN SANDAL",
+                    branch:"NEW BALANCE",
+                    shortDesc:"",
+                    price:'1.380.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'SANDALS_NEW_1.jpg'
+                        },
+                        {
+                            src:'SANDALS_NEW_2.jpg'
+                        }
+                    ]
+                }
+            ]
+        },
+        accessories: {
+            bags:[
+                {
+                    name:"NIKE CHEYENNE 3.0 SOLID",
+                    branch:"NIKE",
+                    shortDesc:"BACKPACK",
+                    price:'2.090.000',
+                    color:"Dark Grey",
+                    description: "",
+                    img: [
+                        {
+                            src:'BAGS_Nike_1.jpg'
+                        },
+                        {
+                            src:'BAGS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            hats:[
+                {
+                    name:"NIKE AEROBILL",
+                    branch:"NIKE",
+                    shortDesc:"RUNNING HAT",
+                    price:'590.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'HATS_Nike_1.jpg'
+                        },
+                        {
+                            src:'HATS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            socks:[
+                {
+                    name:"NIKEGRIP ELITE LIGHTWEIGHT NO-SHOW",
+                    branch:"NIKE",
+                    shortDesc:"RUNNING SOCKS",
+                    price:'790.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'SOCKS_Nike_1.jpg'
+                        },
+                        {
+                            src:'SOCKS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ]
+        },
+        apparel:{
+            tshirt:[
+                {
+                    name:"NIKE FUTURA ICON",
+                    branch:"NIKE",
+                    shortDesc:"MEN'S T-SHIRT",
+                    price:'890.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'T-SHIRT_Nike_1.jpg'
+                        },
+                        {
+                            src:'T-SHIRT_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            shorts:[
+                {
+                    name:"NIKE AEROSWIFT",
+                    branch:"NIKE",
+                    shortDesc:"MEN'S 5\" (12.5CM APPROX.) RUNNING SHORTS",
+                    price:'1.990.000',
+                    color:"Black",
+                    description: "",
+                    img: [
+                        {
+                            src:'SHORTS_Nike_1.jpg'
+                        },
+                        {
+                            src:'SHORTS_Nike_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            hoodie:[
+                {
+                    name:"CAMOUFLAGE BLOCKED HOODIE",
+                    branch:"ADIDAS",
+                    shortDesc:"",
+                    price:'1.250.000',
+                    color:"Black (AY8891)",
+                    description: "",
+                    img: [
+                        {
+                            src:'HOODIES_ADIDAS_1.jpg'
+                        },
+                        {
+                            src:'HOODIES_ADIDAS_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            jackets:[
+                {
+                    name:"MEN'S PUFFER JACKET",
+                    branch:"ASICS",
+                    shortDesc:"",
+                    price:'1.200.000',
+                    color:"DARK GREY",
+                    description: "",
+                    img: [
+                        {
+                            src:'JACKET_ASICS_1.jpg'
+                        },
+                        {
+                            src:'JACKET_ASICS_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            pants:[
+                {
+                    name:"RUNNING LEGGING",
+                    branch:"REEBOK ",
+                    shortDesc:"",
+                    price:'1.200.000',
+                    color:"Black (B47127)",
+                    description: "",
+                    img: [
+                        {
+                            src:'PANTS_REEBOK_1.jpg'
+                        },
+                        {
+                            src:'PANTS_REEBOK_2.jpg'
+                        }
+                    ]
+                }
+            ],
+            joggers:[
+                {
+                    name:"WORKOUT READY BIG LOGO COTTON PANT",
+                    branch:"REEBOK ",
+                    shortDesc:"",
+                    price:'960.000',
+                    color:"Black (B49898)",
+                    description: "",
+                    img: [
+                        {
+                            src:'JOGGER_REEBOK_1.jpg'
+                        },
+                        {
+                            src:'JOGGER_REEBOK_2.jpg'
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+}).call();
--- js/women.js
+++ js/women.js
@@ -1,3 +1,29 @@
 /**
  * Created by HuyTCM on 1/14/17.
  */
+(function () {
+
+    var WOMEN_ITEM = {
+        shoes : {
+            soccer:[],
+            tennis:[],
+            training:[],
+            running:[],
+            sandals:[]
+        },
+        accessories: {
+            bags:[],
+            hats:[],
+            socks:[]
+        },
+        apparel:{
+            tshirt:[],
+            shorts:[],
+            hoodie:[],
+            jackets:[],
+            pants:[],
+            joggers:[]
+        }
+    }
+
+}).call();
